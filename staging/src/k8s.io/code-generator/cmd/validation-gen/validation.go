26ce071c3e04e141788a8be51c440f62128e012a
yongruilin <yongrlin@outlook.com>
2025-05-12 22:47:45 +0000

    feat: add default ratcheting support
    Emit equivalence check before emit validators when needed.

--- staging/src/k8s.io/code-generator/cmd/validation-gen/validation.go
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/validation.go
@@ -1778,6 +1778,8 @@ func isDirectComparable(t *types.Type) bool {
 			}
 		}
 		return true
+	case types.Array:
+		return isDirectComparable(t.Elem)
 	case types.Alias:
 		return isDirectComparable(t.Underlying)
 	}
