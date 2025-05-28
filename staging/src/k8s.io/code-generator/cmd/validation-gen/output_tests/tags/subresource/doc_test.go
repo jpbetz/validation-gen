d3b6969974f738a1c179c32f091270bb7a5f4f0d
Joe Betz <jpbetz@google.com>
2025-05-28 15:45:30 -0400

    Add output tests

---
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/subresource/doc_test.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package subresource
+
+import (
+	"testing"
+)
+
+func Test(t *testing.T) {
+	st := localSchemeBuilder.Test(t)
+
+	st.Value(&Struct{
+		// All zero values
+	}).ExpectRegexpsByPath(map[string][]string{
+		"xDisabledField": []string{"field Struct.XDisabledField"},
+		"yDisabledField": []string{"field Struct.YDisabledField"},
+	})
+
+	st.Value(&Struct{
+		// All zero values
+	}).Subresources([]string{"x"}).ExpectRegexpsByPath(map[string][]string{
+		"xEnabledField":  []string{"field Struct.XEnabledField"},
+		"yDisabledField": []string{"field Struct.YDisabledField"},
+	})
+
+	st.Value(&Struct{
+		// All zero values
+	}).Subresources([]string{"y"}).ExpectRegexpsByPath(map[string][]string{
+		"xDisabledField": []string{"field Struct.XDisabledField"},
+		"yEnabledField":  []string{"field Struct.YEnabledField"},
+	})
+}
