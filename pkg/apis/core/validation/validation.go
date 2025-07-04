thockin/kubernetes

WIP: Add an alt path to objectMeta validation
Before: Name validation functions return `[]string` and apimachinery
logic wraps those in `field.Invalid()`.

Problem: This makes it hard to incrementally add declarative validation,
which needs access to potential name and generateName errors.

After: New name validation functions return `field.ErrorList`, which can
be wrapped in trivial functions to do things like "mark as covered by
declarative" in just one place rather than for all types.

e.g.:

```
-       allErrs := ValidateObjectMeta(&controller.ObjectMeta, true, ValidateReplicationControllerName, field.NewPath("metadata"))
+       allErrs := ValidateObjectMeta2(&controller.ObjectMeta, true, apimachineryvalidation.ValidateNameAsDNSSubdomain, field.NewPath("metadata"))
```

and then:

```
-       allErrs := ValidateObjectMeta2(&controller.ObjectMeta, true, apimachineryvalidation.ValidateNameAsDNSSubdomain, field.NewPath("metadata"))
+       namefn := func(fldPath *field.Path, name string, prefix bool) field.ErrorList {
+               return apimachineryvalidation.ValidateNameAsDNSSubdomain(field.NewPath("metadata"), name, prefix).MarkCoveredByDeclarative()
+       }
+       allErrs := ValidateObjectMeta2(&controller.ObjectMeta, true, namefn, field.NewPath("metadata"))
```

WIP:
1) The `ValidateObjectMeta2` name style is a placeholder for discussion.
2) If we are open to a slightly larger diff and a "breaking" change in
   apimachinery, we could just change the signature and fix everywhere
   that calls this.
3) Rather than add funcs like `ValidateNameAsDNSSubdomain()` we could
   make callers say something like
   `validate.GenerateName(validate.DNSSubdomain)` which would return a
   function of the new singature which applies the generate-name prefix
   logic.  We probably need this for declarative anyway.

pkg/apis/core/validation/validation.go
@@ -251,6 +251,12 @@ func ValidateEndpointsSpecificAnnotations(annotations map[string]string, fldPath
 // value that were not valid.  Otherwise this returns an empty list or nil.
 type ValidateNameFunc apimachineryvalidation.ValidateNameFunc
 
+// ValidateNameFunc2 validates that the provided name is valid for a given
+// resource type. Prefix is true if the name will have a value appended to it.
+//
+// This is similar to ValidateNameFunc, except for the return type.
+type ValidateNameFunc2 = apimachineryvalidation.ValidateNameFunc2
+
 // ValidatePodName can be used to check whether the given pod name is valid.
 // Prefix indicates this name will be used as part of generation, in which case
 // trailing dashes are allowed.
@@ -381,7 +387,31 @@ func ValidateImmutableAnnotation(newVal string, oldVal string, annotation string
 // It doesn't return an error for rootscoped resources with namespace, because namespace should already be cleared before.
 // TODO: Remove calls to this method scattered in validations of specific resources, e.g., ValidatePodUpdate.
 func ValidateObjectMeta(meta *metav1.ObjectMeta, requiresNamespace bool, nameFn ValidateNameFunc, fldPath *field.Path) field.ErrorList {
-	allErrs := apimachineryvalidation.ValidateObjectMeta(meta, requiresNamespace, apimachineryvalidation.ValidateNameFunc(nameFn), fldPath)
+	callValidateObjMeta := func() field.ErrorList {
+		return apimachineryvalidation.ValidateObjectMeta(meta, requiresNamespace, apimachineryvalidation.ValidateNameFunc(nameFn), fldPath)
+	}
+	return validateObjectMetaInner(meta, callValidateObjMeta, fldPath)
+}
+
+// ValidateObjectMeta2 validates an object's metadata on creation. It expects
+// that name generation has already been performed. It doesn't return an error
+// for rootscoped resources with namespace, because namespace should already be
+// cleared.
+//
+// This is similar to ValidateObjectMeta, but the nameFn argument returns a
+// field.ErrorList.
+func ValidateObjectMeta2(meta *metav1.ObjectMeta, requiresNamespace bool, nameFn ValidateNameFunc2, fldPath *field.Path) field.ErrorList {
+	callValidateObjMeta := func() field.ErrorList {
+		return apimachineryvalidation.ValidateObjectMeta2(meta, requiresNamespace, nameFn, fldPath)
+	}
+	return validateObjectMetaInner(meta, callValidateObjMeta, fldPath)
+}
+
+// implements both forms of ValidateObjectMeta.
+func validateObjectMetaInner(meta *metav1.ObjectMeta, callValidateObjMeta func() field.ErrorList, fldPath *field.Path) field.ErrorList {
+	// do most of the objMeta validation
+	allErrs := callValidateObjMeta()
+
 	// run additional checks for the finalizer name
 	for i := range meta.Finalizers {
 		allErrs = append(allErrs, validateKubeFinalizerName(string(meta.Finalizers[i]), fldPath.Child("finalizers").Index(i))...)
@@ -6247,7 +6277,7 @@ func ValidateServiceStatusUpdate(service, oldService *core.Service) field.ErrorL
 
 // ValidateReplicationController tests if required fields in the replication controller are set.
 func ValidateReplicationController(controller *core.ReplicationController, opts PodValidationOptions) field.ErrorList {
-	allErrs := ValidateObjectMeta(&controller.ObjectMeta, true, apimachineryvalidation.NameIsDNSSubdomain, field.NewPath("metadata"))
+	allErrs := ValidateObjectMeta2(&controller.ObjectMeta, true, apimachineryvalidation.ValidateNameAsDNSSubdomain, field.NewPath("metadata"))
 	allErrs = append(allErrs, ValidateReplicationControllerSpec(&controller.Spec, nil, field.NewPath("spec"), opts)...)
 	return allErrs
 }

staging/src/k8s.io/apimachinery/pkg/api/validation/generic.go
@@ -17,8 +17,11 @@ limitations under the License.
 package validation
 
 import (
+	"context"
 	"strings"
 
+	"k8s.io/apimachinery/pkg/api/operation"
+	"k8s.io/apimachinery/pkg/api/validate"
 	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
@@ -33,12 +36,29 @@ const IsNegativeErrorMsg string = `must be greater than or equal to 0`
 // value that were not valid.  Otherwise this returns an empty list or nil.
 type ValidateNameFunc func(name string, prefix bool) []string
 
+// ValidateNameFunc2 validates that the provided name is valid for a given
+// resource type. Prefix is true if the name will have a value appended to it.
+//
+// This is similar to ValidateNameFunc, except that it produces a
+// field.ErrorList.
+type ValidateNameFunc2 func(fldPath *field.Path, name string, prefix bool) field.ErrorList
+
 // NameIsDNSSubdomain is a ValidateNameFunc for names that must be a DNS subdomain.
 func NameIsDNSSubdomain(name string, prefix bool) []string {
 	if prefix {
 		name = maskTrailingDash(name)
 	}
 	return validation.IsDNS1123Subdomain(name)
+}
+
+// ValidateNameAsDNSSubdomain is a ValidateNameFunc2 for names that must be a DNS subdomain.
+// This is a bridge between the older "imperative" validation style and the
+// newer "declarative" validation style.
+func ValidateNameAsDNSSubdomain(fldPath *field.Path, name string, prefix bool) field.ErrorList {
+	if prefix {
+		name = maskTrailingDash(name)
+	}
+	return validate.DNSSubdomain(context.Background(), operation.Operation{}, fldPath, &name, nil)
 }
 
 // NameIsDNSLabel is a ValidateNameFunc for names that must be a DNS 1123 label.

jpbetz/validation-gen

pkg/apis/core/validation/validation.go
@@ -251,6 +251,12 @@ func ValidateEndpointsSpecificAnnotations(annotations map[string]string, fldPath
 // value that were not valid.  Otherwise this returns an empty list or nil.
 type ValidateNameFunc apimachineryvalidation.ValidateNameFunc
 
+// ValidateNameFunc2 validates that the provided name is valid for a given
+// resource type. Prefix is true if the name will have a value appended to it.
+//
+// This is similar to ValidateNameFunc, except for the return type.
+type ValidateNameFunc2 = apimachineryvalidation.ValidateNameFunc2
+
 // ValidatePodName can be used to check whether the given pod name is valid.
 // Prefix indicates this name will be used as part of generation, in which case
 // trailing dashes are allowed.
@@ -381,7 +387,31 @@ func ValidateImmutableAnnotation(newVal string, oldVal string, annotation string
 // It doesn't return an error for rootscoped resources with namespace, because namespace should already be cleared before.
 // TODO: Remove calls to this method scattered in validations of specific resources, e.g., ValidatePodUpdate.
 func ValidateObjectMeta(meta *metav1.ObjectMeta, requiresNamespace bool, nameFn ValidateNameFunc, fldPath *field.Path) field.ErrorList {
-	allErrs := apimachineryvalidation.ValidateObjectMeta(meta, requiresNamespace, apimachineryvalidation.ValidateNameFunc(nameFn), fldPath)
+	callValidateObjMeta := func() field.ErrorList {
+		return apimachineryvalidation.ValidateObjectMeta(meta, requiresNamespace, apimachineryvalidation.ValidateNameFunc(nameFn), fldPath)
+	}
+	return validateObjectMetaInner(meta, callValidateObjMeta, fldPath)
+}
+
+// ValidateObjectMeta2 validates an object's metadata on creation. It expects
+// that name generation has already been performed. It doesn't return an error
+// for rootscoped resources with namespace, because namespace should already be
+// cleared.
+//
+// This is similar to ValidateObjectMeta, but the nameFn argument returns a
+// field.ErrorList.
+func ValidateObjectMeta2(meta *metav1.ObjectMeta, requiresNamespace bool, nameFn ValidateNameFunc2, fldPath *field.Path) field.ErrorList {
+	callValidateObjMeta := func() field.ErrorList {
+		return apimachineryvalidation.ValidateObjectMeta2(meta, requiresNamespace, nameFn, fldPath)
+	}
+	return validateObjectMetaInner(meta, callValidateObjMeta, fldPath)
+}
+
+// implements both forms of ValidateObjectMeta.
+func validateObjectMetaInner(meta *metav1.ObjectMeta, callValidateObjMeta func() field.ErrorList, fldPath *field.Path) field.ErrorList {
+	// do most of the objMeta validation
+	allErrs := callValidateObjMeta()
+
 	// run additional checks for the finalizer name
 	for i := range meta.Finalizers {
 		allErrs = append(allErrs, validateKubeFinalizerName(string(meta.Finalizers[i]), fldPath.Child("finalizers").Index(i))...)
@@ -6305,7 +6335,7 @@ func ValidateServiceStatusUpdate(service, oldService *core.Service) field.ErrorL
 
 // ValidateReplicationController tests if required fields in the replication controller are set.
 func ValidateReplicationController(controller *core.ReplicationController, opts PodValidationOptions) field.ErrorList {
-	allErrs := ValidateObjectMeta(&controller.ObjectMeta, true, apimachineryvalidation.NameIsDNSSubdomain, field.NewPath("metadata"))
+	allErrs := ValidateObjectMeta2(&controller.ObjectMeta, true, apimachineryvalidation.ValidateNameAsK8sLongName, field.NewPath("metadata"))
 	allErrs = append(allErrs, ValidateReplicationControllerSpec(&controller.Spec, nil, field.NewPath("spec"), opts)...)
 	return allErrs
 }

staging/src/k8s.io/apimachinery/pkg/api/validation/generic.go
@@ -17,8 +17,11 @@ limitations under the License.
 package validation
 
 import (
+	"context"
 	"strings"
 
+	"k8s.io/apimachinery/pkg/api/operation"
+	"k8s.io/apimachinery/pkg/api/validate"
 	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 )
@@ -33,12 +36,29 @@ const IsNegativeErrorMsg string = `must be greater than or equal to 0`
 // value that were not valid.  Otherwise this returns an empty list or nil.
 type ValidateNameFunc func(name string, prefix bool) []string
 
+// ValidateNameFunc2 validates that the provided name is valid for a given
+// resource type. Prefix is true if the name will have a value appended to it.
+//
+// This is similar to ValidateNameFunc, except that it produces a
+// field.ErrorList.
+type ValidateNameFunc2 func(fldPath *field.Path, name string, prefix bool) field.ErrorList
+
 // NameIsDNSSubdomain is a ValidateNameFunc for names that must be a DNS subdomain.
 func NameIsDNSSubdomain(name string, prefix bool) []string {
 	if prefix {
 		name = maskTrailingDash(name)
 	}
 	return validation.IsDNS1123Subdomain(name)
+}
+
+// ValidateNameAsK8sLongName is a ValidateNameFunc2 for names that must be a DNS subdomain.
+// This is a bridge between the older "imperative" validation style and the
+// newer "declarative" validation style.
+func ValidateNameAsK8sLongName(fldPath *field.Path, name string, prefix bool) field.ErrorList {
+	if prefix {
+		name = maskTrailingDash(name)
+	}
+	return validate.LongName(context.Background(), operation.Operation{}, fldPath, &name, nil)
 }
 
 // NameIsDNSLabel is a ValidateNameFunc for names that must be a DNS 1123 label.