thockin/kubernetes

Add DNS subdomain format

staging/src/k8s.io/apimachinery/pkg/api/validate/strfmt_test.go
@@ -103,3 +103,102 @@ func TestDNS1123Label(t *testing.T) {
 		})
 	}
 }
+
+func TestDNS1123Subdomain(t *testing.T) {
+	ctx := context.Background()
+	fldPath := field.NewPath("test")
+
+	testCases := []struct {
+		name     string
+		input    string
+		wantErrs field.ErrorList
+	}{{
+		name:     "valid single label",
+		input:    "valid-label",
+		wantErrs: nil,
+	}, {
+		name:     "valid subdomain",
+		input:    "this-is.a-valid.subdomain",
+		wantErrs: nil,
+	}, {
+		name:     "valid single character elements",
+		input:    "a.b.c",
+		wantErrs: nil,
+	}, {
+		name:     "valid elements with numbers",
+		input:    "123.abc-123.456-def",
+		wantErrs: nil,
+	}, {
+		name:     "all number elements",
+		input:    "1.2.3.4",
+		wantErrs: nil,
+	}, {
+		name:  "invalid: uppercase characters",
+		input: "Invalid.Subdomain",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=dns-subdomain"),
+		},
+	}, {
+		name:  "invalid: starts with dash",
+		input: "this-is.-an-invalid.subdomain",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=dns-subdomain"),
+		},
+	}, {
+		name:  "invalid: ends with dash",
+		input: "this-is.an-invalid-.subdomain",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=dns-subdomain"),
+		},
+	}, {
+		name:  "invalid: contains double dots",
+		input: "invalid..subdomain",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=dns-subdomain"),
+		},
+	}, {
+		name:  "invalid: contains special characters",
+		input: "inv@lid.subdoma!n",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=dns-subdomain"),
+		},
+	}, {
+		name:  "invalid: too long single label",
+		input: "a" + strings.Repeat("b", 252) + "c", // 254 characters
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=dns-subdomain"),
+		},
+	}, {
+		name: "invalid: too long multiple labels",
+		input: strings.Join([]string{
+			strings.Repeat("a", 60), // 61 with the "."
+			strings.Repeat("b", 60), // 122 with the "."
+			strings.Repeat("c", 60), // 183 with the "."
+			strings.Repeat("d", 60), // 244 with the "."
+			strings.Repeat("e", 10), // 254 characters
+		}, "."),
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=dns-subdomain"),
+		},
+	}, {
+		name:     "valid: max length single label",     // supported for compat
+		input:    "a" + strings.Repeat("b", 251) + "c", // 253 characters
+		wantErrs: nil,
+	}, {
+		name:  "invalid: empty string",
+		input: "",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=dns-subdomain"),
+		},
+	}}
+
+	matcher := field.ErrorMatcher{}.ByType().ByField().ByOrigin()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			value := tc.input
+			gotErrs := DNSSubdomain(ctx, operation.Operation{}, fldPath, &value, nil)
+
+			matcher.Test(t, tc.wantErrs, gotErrs)
+		})
+	}
+}

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/format/zz_generated.validations.go
@@ -53,6 +53,13 @@ func Validate_DNSLabelStringType(ctx context.Context, op operation.Operation, fl
 	return errs
 }
 
+func Validate_DNSSubdomainStringType(ctx context.Context, op operation.Operation, fldPath *field.Path, obj, oldObj *DNSSubdomainStringType) (errs field.ErrorList) {
+	// type DNSSubdomainStringType
+	errs = append(errs, validate.DNSSubdomain(ctx, op, fldPath, obj, oldObj)...)
+
+	return errs
+}
+
 func Validate_IPStringType(ctx context.Context, op operation.Operation, fldPath *field.Path, obj, oldObj *IPStringType) (errs field.ErrorList) {
 	// type IPStringType
 	errs = append(errs, validate.IPSloppy(ctx, op, fldPath, obj, oldObj)...)
@@ -104,6 +111,27 @@ func Validate_Struct(ctx context.Context, op operation.Operation, fldPath *field
 			errs = append(errs, Validate_DNSLabelStringType(ctx, op, fldPath, obj, oldObj)...)
 			return
 		}(fldPath.Child("dnsLabelTypedefField"), &obj.DNSLabelTypedefField, safe.Field(oldObj, func(oldObj *Struct) *DNSLabelStringType { return &oldObj.DNSLabelTypedefField }))...)
+
+	// field Struct.DNSSubdomainField
+	errs = append(errs,
+		func(fldPath *field.Path, obj, oldObj *string) (errs field.ErrorList) {
+			errs = append(errs, validate.DNSSubdomain(ctx, op, fldPath, obj, oldObj)...)
+			return
+		}(fldPath.Child("dnsSubdomainField"), &obj.DNSSubdomainField, safe.Field(oldObj, func(oldObj *Struct) *string { return &oldObj.DNSSubdomainField }))...)
+
+	// field Struct.DNSSubdomainPtrField
+	errs = append(errs,
+		func(fldPath *field.Path, obj, oldObj *string) (errs field.ErrorList) {
+			errs = append(errs, validate.DNSSubdomain(ctx, op, fldPath, obj, oldObj)...)
+			return
+		}(fldPath.Child("dnsSubdomainPtrField"), obj.DNSSubdomainPtrField, safe.Field(oldObj, func(oldObj *Struct) *string { return oldObj.DNSSubdomainPtrField }))...)
+
+	// field Struct.DNSSubdomainTypedefField
+	errs = append(errs,
+		func(fldPath *field.Path, obj, oldObj *DNSSubdomainStringType) (errs field.ErrorList) {
+			errs = append(errs, Validate_DNSSubdomainStringType(ctx, op, fldPath, obj, oldObj)...)
+			return
+		}(fldPath.Child("dnsSubdomainTypedefField"), &obj.DNSSubdomainTypedefField, safe.Field(oldObj, func(oldObj *Struct) *DNSSubdomainStringType { return &oldObj.DNSSubdomainTypedefField }))...)
 
 	return errs
 }

jpbetz/validation-gen

staging/src/k8s.io/apimachinery/pkg/api/validate/strfmt_test.go
@@ -103,3 +103,102 @@ func TestShortName(t *testing.T) {
 		})
 	}
 }
+
+func TestLongName(t *testing.T) {
+	ctx := context.Background()
+	fldPath := field.NewPath("test")
+
+	testCases := []struct {
+		name     string
+		input    string
+		wantErrs field.ErrorList
+	}{{
+		name:     "valid single label",
+		input:    "valid-label",
+		wantErrs: nil,
+	}, {
+		name:     "valid subdomain",
+		input:    "this-is.a-valid.subdomain",
+		wantErrs: nil,
+	}, {
+		name:     "valid single character elements",
+		input:    "a.b.c",
+		wantErrs: nil,
+	}, {
+		name:     "valid elements with numbers",
+		input:    "123.abc-123.456-def",
+		wantErrs: nil,
+	}, {
+		name:     "all number elements",
+		input:    "1.2.3.4",
+		wantErrs: nil,
+	}, {
+		name:  "invalid: uppercase characters",
+		input: "Invalid.Subdomain",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=k8s-long-name"),
+		},
+	}, {
+		name:  "invalid: starts with dash",
+		input: "this-is.-an-invalid.subdomain",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=k8s-long-name"),
+		},
+	}, {
+		name:  "invalid: ends with dash",
+		input: "this-is.an-invalid-.subdomain",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=k8s-long-name"),
+		},
+	}, {
+		name:  "invalid: contains double dots",
+		input: "invalid..subdomain",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=k8s-long-name"),
+		},
+	}, {
+		name:  "invalid: contains special characters",
+		input: "inv@lid.subdoma!n",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=k8s-long-name"),
+		},
+	}, {
+		name:  "invalid: too long single label",
+		input: "a" + strings.Repeat("b", 252) + "c", // 254 characters
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=k8s-long-name"),
+		},
+	}, {
+		name: "invalid: too long multiple labels",
+		input: strings.Join([]string{
+			strings.Repeat("a", 60), // 61 with the "."
+			strings.Repeat("b", 60), // 122 with the "."
+			strings.Repeat("c", 60), // 183 with the "."
+			strings.Repeat("d", 60), // 244 with the "."
+			strings.Repeat("e", 10), // 254 characters
+		}, "."),
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=k8s-long-name"),
+		},
+	}, {
+		name:     "valid: max length single label",     // supported for compat
+		input:    "a" + strings.Repeat("b", 251) + "c", // 253 characters
+		wantErrs: nil,
+	}, {
+		name:  "invalid: empty string",
+		input: "",
+		wantErrs: field.ErrorList{
+			field.Invalid(fldPath, nil, "").WithOrigin("format=k8s-long-name"),
+		},
+	}}
+
+	matcher := field.ErrorMatcher{}.ByType().ByField().ByOrigin()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			value := tc.input
+			gotErrs := LongName(ctx, operation.Operation{}, fldPath, &value, nil)
+
+			matcher.Test(t, tc.wantErrs, gotErrs)
+		})
+	}
+}

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/format/zz_generated.validations.go
@@ -57,6 +57,16 @@ func Validate_IPStringType(ctx context.Context, op operation.Operation, fldPath 
 	return errs
 }
 
+func Validate_LongNameStringType(ctx context.Context, op operation.Operation, fldPath *field.Path, obj, oldObj *LongNameStringType) (errs field.ErrorList) {
+	// type LongNameStringType
+	if op.Type == operation.Update && (obj == oldObj || (obj != nil && oldObj != nil && *obj == *oldObj)) {
+		return nil // no changes
+	}
+	errs = append(errs, validate.LongName(ctx, op, fldPath, obj, oldObj)...)
+
+	return errs
+}
+
 func Validate_ShortNameStringType(ctx context.Context, op operation.Operation, fldPath *field.Path, obj, oldObj *ShortNameStringType) (errs field.ErrorList) {
 	// type ShortNameStringType
 	if op.Type == operation.Update && (obj == oldObj || (obj != nil && oldObj != nil && *obj == *oldObj)) {
@@ -123,6 +133,33 @@ func Validate_Struct(ctx context.Context, op operation.Operation, fldPath *field
 			errs = append(errs, Validate_ShortNameStringType(ctx, op, fldPath, obj, oldObj)...)
 			return
 		}(fldPath.Child("shortNameTypedefField"), &obj.ShortNameTypedefField, safe.Field(oldObj, func(oldObj *Struct) *ShortNameStringType { return &oldObj.ShortNameTypedefField }))...)
+
+	// field Struct.LongNameField
+	errs = append(errs,
+		func(fldPath *field.Path, obj, oldObj *string) (errs field.ErrorList) {
+			if op.Type == operation.Update && (obj == oldObj || (obj != nil && oldObj != nil && *obj == *oldObj)) {
+				return nil // no changes
+			}
+			errs = append(errs, validate.LongName(ctx, op, fldPath, obj, oldObj)...)
+			return
+		}(fldPath.Child("longNameField"), &obj.LongNameField, safe.Field(oldObj, func(oldObj *Struct) *string { return &oldObj.LongNameField }))...)
+
+	// field Struct.LongNamePtrField
+	errs = append(errs,
+		func(fldPath *field.Path, obj, oldObj *string) (errs field.ErrorList) {
+			if op.Type == operation.Update && (obj == oldObj || (obj != nil && oldObj != nil && *obj == *oldObj)) {
+				return nil // no changes
+			}
+			errs = append(errs, validate.LongName(ctx, op, fldPath, obj, oldObj)...)
+			return
+		}(fldPath.Child("longNamePtrField"), obj.LongNamePtrField, safe.Field(oldObj, func(oldObj *Struct) *string { return oldObj.LongNamePtrField }))...)
+
+	// field Struct.LongNameTypedefField
+	errs = append(errs,
+		func(fldPath *field.Path, obj, oldObj *LongNameStringType) (errs field.ErrorList) {
+			errs = append(errs, Validate_LongNameStringType(ctx, op, fldPath, obj, oldObj)...)
+			return
+		}(fldPath.Child("longNameTypedefField"), &obj.LongNameTypedefField, safe.Field(oldObj, func(oldObj *Struct) *LongNameStringType { return &oldObj.LongNameTypedefField }))...)
 
 	return errs
 }