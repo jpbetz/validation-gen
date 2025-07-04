thockin/kubernetes

Add support for validation cohorts
On a given field or type, there may be multiple "cohorts" of validation.
The unnamed cohort (aka default cohort) is the first.  Any named cohorts
are emitted as inline functions, so early-return can be used.

This allows k8s:subfield to be used to generate cohorts (named after the
field's JSON name) which behave properly, For example:

```
    // +k8s:subfield(name)=+k8s:optional
    // +k8s:subfield(name)=+k8s:format=dns-label
    // +k8s:subfield(generateName)=+k8s:optional
    // +k8s:subfield(generateName)=+k8s:format=dns-label
    metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
```

...will generate:

```
       errs = append(errs,
               func(fldPath *field.Path, obj, oldObj *metav1.ObjectMeta) (errs field.ErrorList) {
                       func() { // cohort name
                               if e := validate.Subfield(ctx, op, fldPath, obj, oldObj, "name", func(o *metav1.ObjectMeta) *string { return &o.Name }, validate.OptionalValue); len (e) != 0 {
                                       return // do not proceed
                               }
                               errs = append(errs, validate.Subfield(ctx, op, fldPath, obj, oldObj, "name", func(o *metav1.ObjectMeta) *string { return &o.Name }, validate.DNSLabel)...)
                       }()
                       func() { // cohort generateName
                               if e := validate.Subfield(ctx, op, fldPath, obj, oldObj, "generateName", func(o *metav1.ObjectMeta) *string { return &o.GenerateName }, validate.OptionalValue); len(e) != 0 {
                                       return // do not proceed
                               }
                               errs = append(errs, validate.Subfield(ctx, op, fldPath, obj, oldObj, "generateName", func(o *metav1.ObjectMeta) *string { return &o.GenerateName }, validate.DNSLabel)...)
                       }()
                       return
               }(fldPa

staging/src/k8s.io/code-generator/cmd/validation-gen/validators/testing.go
@@ -73,7 +73,10 @@ func (frtv fixedResultTagValidator) GetValidations(context Context, _ []string, 
 	if err != nil {
 		return result, fmt.Errorf("can't decode tag payload: %w", err)
 	}
-	result.AddFunction(Function(frtv.TagName(), tag.flags, fixedResultValidator, frtv.result, tag.msg).WithTypeArgs(tag.typeArgs...))
+	fg := Function(frtv.TagName(), tag.flags, fixedResultValidator, frtv.result, tag.msg)
+	fg.TypeArgs = tag.typeArgs
+	fg.Cohort = tag.cohort
+	result.AddFunction(fg)
 
 	return result, nil
 }
@@ -86,6 +89,7 @@ type fixedResultPayload struct {
 	flags    FunctionFlags
 	msg      string
 	typeArgs []types.Name
+	cohort   string
 }
 
 func (fixedResultTagValidator) parseTagPayload(in string) (fixedResultPayload, error) {
@@ -93,6 +97,7 @@ func (fixedResultTagValidator) parseTagPayload(in string) (fixedResultPayload, e
 		Flags   []string `json:"flags"`
 		Msg     string   `json:"msg"`
 		TypeArg string   `json:"typeArg,omitempty"`
+		Cohort  string   `json:"cohort,omitempty"`
 	}
 	// We expect either a string (maybe empty) or a JSON object.
 	if len(in) == 0 {
@@ -129,7 +134,12 @@ func (fixedResultTagValidator) parseTagPayload(in string) (fixedResultPayload, e
 		typeArgs = []types.Name{{Package: "", Name: tn}}
 	}
 
-	return fixedResultPayload{flags, pl.Msg, typeArgs}, nil
+	return fixedResultPayload{
+		flags:    flags,
+		msg:      pl.Msg,
+		typeArgs: typeArgs,
+		cohort:   pl.Cohort,
+	}, nil
 }
 
 func (frtv fixedResultTagValidator) Docs() TagDoc {
@@ -165,6 +175,10 @@ func (frtv fixedResultTagValidator) Docs() TagDoc {
 				Key:   "typeArg",
 				Value: "<string>",
 				Docs:  "The type arg in generated code (must be the value-type, not pointer).",
+			}, {
+				Key:   "cohort",
+				Value: "<string>",
+				Docs:  "An optional cohort name to group multiple validations.",
 			}},
 		}}
 		if frtv.result {

jpbetz/validation-gen

  errs = append(errs,
    func(fldPath *field.Path, obj, oldObj *metav1.ObjectMeta) (errs field.ErrorList) {
      func() { // cohort name
        if e := validate.Subfield(ctx, op, fldPath, obj, oldObj, "name", func(o *metav1.ObjectMeta) *string { return &o.Name }, validate.OptionalValue); len (e) != 0 {
          return // do not proceed
        }
        errs = append(errs, validate.Subfield(ctx, op, fldPath, obj, oldObj, "name", func(o *metav1.ObjectMeta) *string { return &o.Name }, validate.DNSLabel)...)
      }()
      func() { // cohort generateName
        if e := validate.Subfield(ctx, op, fldPath, obj, oldObj, "generateName", func(o *metav1.ObjectMeta) *string { return &o.GenerateName }, validate.OptionalValue); len(e) != 0 {
        errs = append(errs, validate.Subfield(ctx, op, fldPath, obj, oldObj, "generateName", func(o *metav1.ObjectMeta) *string { return &o.GenerateName }, validate.DNSLabel)...)
      return
    }(fldPath.Child("metadata"), &obj.ObjectMeta, safe.Field(oldObj, func(oldObj *corev1.ReplicationController) *metav1.ObjectMeta { return &oldObj.ObjectMeta }))...)

staging/src/k8s.io/code-generator/cmd/validation-gen/validators/testing.go
@@ -73,7 +73,9 @@ func (frtv fixedResultTagValidator) GetValidations(context Context, tag codetags
 	if err != nil {
 		return result, fmt.Errorf("can't decode tag payload: %w", err)
 	}
-	result.AddFunction(Function(frtv.TagName(), args.flags, fixedResultValidator, frtv.result, args.msg).WithTypeArgs(args.typeArgs...))
+	fn := Function(frtv.TagName(), args.flags, fixedResultValidator, frtv.result, args.msg).WithTypeArgs(args.typeArgs...)
+	fn.Cohort = args.cohort
+	result.AddFunction(fn)
 
 	return result, nil
 }
@@ -86,6 +88,7 @@ type fixedResultArgs struct {
 	flags    FunctionFlags
 	msg      string
 	typeArgs []types.Name
+	cohort   string
 }
 
 func (fixedResultTagValidator) toFixedResultArgs(in codetags.Tag) (fixedResultArgs, error) {
@@ -111,6 +114,8 @@ func (fixedResultTagValidator) toFixedResultArgs(in codetags.Tag) (fixedResultAr
 				}
 				result.typeArgs = []types.Name{{Package: "", Name: tn}}
 			}
+		case "cohort":
+			result.cohort = a.Value
 		}
 	}
 	if in.ValueType == codetags.ValueTypeString {
@@ -148,6 +153,11 @@ func (frtv fixedResultTagValidator) Docs() TagDoc {
 			Name:        "typeArg",
 			Description: "<string>",
 			Docs:        "The type arg in generated code (must be the value-type, not pointer).",
+			Type:        codetags.ArgTypeString,
+		}, {
+			Name:        "cohort",
+			Description: "<string>",
+			Docs:        "An optional cohort name to group multiple validations.",
 			Type:        codetags.ArgTypeString,
 		}}
 		if frtv.result {