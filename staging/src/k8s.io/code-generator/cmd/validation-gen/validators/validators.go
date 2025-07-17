057c9157887c9a8dd73aa0830445430f1a9bcd42
Tim Hockin <thockin@google.com>
2025-07-16 17:44:52 -0700

    Add ListSelector in validation Context
    We already have "Member" which is a way to "forward" information from
    stem validators (each, subfield, item) to leaf validators.  That works
    for fields in a struct but not items in a list.
    
    Now we have the list selection criteria.
    
    This simplifies union a bit and gets rid of the need to regex parse the
    field path.

--- staging/src/k8s.io/code-generator/cmd/validation-gen/validators/validators.go
+++ staging/src/k8s.io/code-generator/cmd/validation-gen/validators/validators.go
@@ -247,6 +247,15 @@ type ListSelectorTerm struct {
 	Value any
 }
 
+// ListSelectorTerm represents a field name and value pair.
+type ListSelectorTerm struct {
+	// Field is the JSON name of the field to match.
+	Field string
+	// Value is the value to match.  This must be a primitive type which can
+	// be used as list-map keys: string, int, or bool.
+	Value any
+}
+
 // TagDoc describes a comment-tag and its usage.
 type TagDoc struct {
 	// Tag is the tag name, without the leading '+'.
