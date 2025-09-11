4c90318568a3046ac56d1bb7c605c9fb454d54ee
yongruilin <yongrlin@outlook.com>
2025-09-11 20:21:22 +0000

    Enable declarative validation support for ResourceClaim/(v1alpha3,v1beta1,v1beta2)

--- pkg/api/testing/validation_test.go
+++ pkg/api/testing/validation_test.go
@@ -35,6 +35,8 @@ func TestVersionedValidationByFuzzing(t *testing.T) {
 		{Group: "certificates.k8s.io", Version: "v1"},
 		{Group: "certificates.k8s.io", Version: "v1alpha1"},
 		{Group: "certificates.k8s.io", Version: "v1beta1"},
+		{Group: "resource.k8s.io", Version: "v1beta1"},
+		{Group: "resource.k8s.io", Version: "v1beta2"},
 		{Group: "resource.k8s.io", Version: "v1"},
 	}
 
