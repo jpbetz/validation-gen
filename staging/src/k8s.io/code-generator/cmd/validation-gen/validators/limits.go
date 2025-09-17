kubernetes/code-generator

Add constant discovery, ScopeConst and StructLiberal. Remove ScopeAny.
# Conflicts:
#	staging/src/k8s.io/code-generator/cmd/validation-gen/validators/limits.go

# Conflicts:
#	staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/options/zz_generated.validations.go

Kubernetes-commit: ed170c1c0aa74c77108c8a809c8ca1897b82f606

cmd/validation-gen/validators/limits.go
@@ -42,9 +42,7 @@ func (minimumTagValidator) TagName() string {
 	return minimumTagName
 }
 
-var minimumTagValidScopes = sets.New(
-	ScopeAny,
-)
+var minimumTagValidScopes = sets.New(ScopeType, ScopeField, ScopeListVal, ScopeMapKey, ScopeMapVal)
 
 func (minimumTagValidator) ValidScopes() sets.Set[Scope] {
 	return minimumTagValidScopes

jpbetz/validation-gen

staging/src/k8s.io/code-generator/cmd/validation-gen/validators/limits.go
@@ -46,7 +46,7 @@ func (maxLengthTagValidator) TagName() string {
 	return maxLengthTagName
 }
 
-var maxLengthTagValidScopes = sets.New(ScopeAny)
+var maxLengthTagValidScopes = sets.New(ScopeType, ScopeField, ScopeListVal, ScopeMapKey, ScopeMapVal)
 
 func (maxLengthTagValidator) ValidScopes() sets.Set[Scope] {
 	return maxLengthTagValidScopes