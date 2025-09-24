thockin/kubernetes

Fix wrong test case for string format

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/format/zz_generated.validations.go
@@ -46,6 +46,13 @@ func RegisterValidations(scheme *testscheme.Scheme) error {
 	return nil
 }
 
+func Validate_DNSLabelStringType(ctx context.Context, op operation.Operation, fldPath *field.Path, obj, oldObj *DNSLabelStringType) (errs field.ErrorList) {
+	// type DNSLabelStringType
+	errs = append(errs, validate.DNSLabel(ctx, op, fldPath, obj, oldObj)...)
+
+	return errs
+}
+
 func Validate_IPStringType(ctx context.Context, op operation.Operation, fldPath *field.Path, obj, oldObj *IPStringType) (errs field.ErrorList) {
 	// type IPStringType
 	errs = append(errs, validate.IPSloppy(ctx, op, fldPath, obj, oldObj)...)
@@ -93,10 +100,10 @@ func Validate_Struct(ctx context.Context, op operation.Operation, fldPath *field
 
 	// field Struct.DNSLabelTypedefField
 	errs = append(errs,
-		func(fldPath *field.Path, obj, oldObj *UnvalidatedStringType) (errs field.ErrorList) {
-			errs = append(errs, validate.DNSLabel(ctx, op, fldPath, obj, oldObj)...)
+		func(fldPath *field.Path, obj, oldObj *DNSLabelStringType) (errs field.ErrorList) {
+			errs = append(errs, Validate_DNSLabelStringType(ctx, op, fldPath, obj, oldObj)...)
 			return
-		}(fldPath.Child("dnsLabelTypedefField"), &obj.DNSLabelTypedefField, safe.Field(oldObj, func(oldObj *Struct) *UnvalidatedStringType { return &oldObj.DNSLabelTypedefField }))...)
+		}(fldPath.Child("dnsLabelTypedefField"), &obj.DNSLabelTypedefField, safe.Field(oldObj, func(oldObj *Struct) *DNSLabelStringType { return &oldObj.DNSLabelTypedefField }))...)
 
 	return errs
 }

jpbetz/validation-gen

staging/src/k8s.io/code-generator/cmd/validation-gen/output_tests/tags/format/zz_generated.validations.go
@@ -48,6 +48,14 @@ func RegisterValidations(scheme *testscheme.Scheme) error {
 	return nil
 }
 
+// Validate_DNSLabelStringType validates an instance of DNSLabelStringType according
+// to declarative validation rules in the API schema.
+func Validate_DNSLabelStringType(ctx context.Context, op operation.Operation, fldPath *field.Path, obj, oldObj *DNSLabelStringType) (errs field.ErrorList) {
+	errs = append(errs, validate.ShortName(ctx, op, fldPath, obj, oldObj)...)
+
+	return errs
+}
+
 // Validate_IPStringType validates an instance of IPStringType according
 // to declarative validation rules in the API schema.
 func Validate_IPStringType(ctx context.Context, op operation.Operation, fldPath *field.Path, obj, oldObj *IPStringType) (errs field.ErrorList) {
@@ -123,15 +131,17 @@ func Validate_Struct(ctx context.Context, op operation.Operation, fldPath *field
 
 	// field Struct.DNSLabelTypedefField
 	errs = append(errs,
-		func(fldPath *field.Path, obj, oldObj *UnvalidatedStringType) (errs field.ErrorList) {
+		func(fldPath *field.Path, obj, oldObj *DNSLabelStringType) (errs field.ErrorList) {
 			// don't revalidate unchanged data
 			if op.Type == operation.Update && (obj == oldObj || (obj != nil && oldObj != nil && *obj == *oldObj)) {
 				return nil
 			}
 			// call field-attached validations
 			errs = append(errs, validate.ShortName(ctx, op, fldPath, obj, oldObj)...)
+			// call the type's validation function
+			errs = append(errs, Validate_DNSLabelStringType(ctx, op, fldPath, obj, oldObj)...)
 			return
-		}(fldPath.Child("dnsLabelTypedefField"), &obj.DNSLabelTypedefField, safe.Field(oldObj, func(oldObj *Struct) *UnvalidatedStringType { return &oldObj.DNSLabelTypedefField }))...)
+		}(fldPath.Child("dnsLabelTypedefField"), &obj.DNSLabelTypedefField, safe.Field(oldObj, func(oldObj *Struct) *DNSLabelStringType { return &oldObj.DNSLabelTypedefField }))...)
 
 	return errs
 }